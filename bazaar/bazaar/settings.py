cb87e823fc87911486d9c43e0930b300e4a54503
Arpit Saxena <arpit.saxena2000@yahoo.in>
2019-03-07 16:24:48 +0530

    Add templates and database access on heroku.

--- bazaar/bazaar/settings.py
+++ bazaar/bazaar/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku    
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,14 +78,7 @@ WSGI_APPLICATION = 'bazaar.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(os.environ['DATABASE_URL'])
 }
 
 
--- index/views.py
+++ index/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Question
+
 def index(request):
-    return HttpResponse("You are at the index of the project")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('index/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
