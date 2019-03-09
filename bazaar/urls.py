fab40d35e458ae72fe213a60ecaf17d626a90381
Arpit Saxena <arpit.saxena2000@yahoo.in>
2019-03-09 15:47:19 +0530

    Remove unused url pattern, remove own unused implementation of signup page

--- bazaar/urls.py
+++ bazaar/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # User Management
-    path('users/', include('users.urls')),
+    # path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -2,21 +2,3 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render, redirect
 
-from .forms import CustomUserCreationForm
-
-""" class SignUp(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
- """
-
-def SignUp(request):
-    if request.method == 'POST':
-        f = CustomUserCreationForm(request.POST)
-        if f.is_valid():
-            f.save()
-            return redirect('login')
-    else:
-        f = CustomUserCreationForm()
-
-    return render(request, 'signup.html', {'form': f})    
